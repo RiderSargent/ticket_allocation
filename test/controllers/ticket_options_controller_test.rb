0de6e9ecb9697c14369068a0cc15e8992d6f4c4d
Rider Sargent <rider.sargent@gmail.com>
2024-10-06 15:44:58 +0100

    Update test descriptions

--- test/controllers/ticket_options_controller_test.rb
+++ test/controllers/ticket_options_controller_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class TicketOptionsControllerTest < ActionDispatch::IntegrationTest
-  test "show action success" do
+  test "/ticket_options/:id returns success" do
     ticket_option = ticket_options(:one)
 
     get "/ticket_options/#{ticket_option.id}"
@@ -9,7 +9,7 @@ class TicketOptionsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "fails with invalid ticket option id" do
+  test "/ticket_options/:id returns 404 for invalid ticket option id" do
     get "/ticket_options/no-such-ticket-option"
 
     assert @response.status = 404
